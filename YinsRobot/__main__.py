93d5a5e09826463ccc673f4945d120df3799c63e
Dappa <94505216+ReyyNada@users.noreply.github.com>
2022-06-30 09:47:18 +0700

    Update __main__.py

--- YinsRobot/__main__.py
+++ YinsRobot/__main__.py
@@ -58,8 +58,9 @@ from telegram.utils.helpers import escape_markdown
 
 
 PM_START_TEXT = """
+(https://telegra.ph/file/6c937e656380c07997dfc.jpg)
 *Hello {} !*
-✪ I'm an ᴢᴇᴀ management bot [✨](https://telegra.ph/file/6c937e656380c07997dfc.jpg)
+✪ I'm an ᴢᴇᴀ management bot [✨]
 ────────────────────────
 × *Uptime:* `{}`
 × `{}` *users, across* `{}` *chats.*
